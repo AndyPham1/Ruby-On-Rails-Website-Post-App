fb9bd46bfdf147dd19216163b531c2da0c218ab4
Andy <andypham2341@hotmail.com>
2016-07-14 19:16:31 -0400

    Pin CtrlUpdate

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,27 @@
 class PinsController < ApplicationController
+	before_action :f_pin, only: [:show,:edit,:update,:destroy]
+
+	def index
+		@pin = Pin.new
+	end
+	def show
+	end
+	def new
+	end
+	def create
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+
+	private
+	def f_pin
+		@pin = Pin.find(params[:id])
+	end
+	def pin_params
+		params.require(:pin).permit(:title,:description)
+	end
 end
