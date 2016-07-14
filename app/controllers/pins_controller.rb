b77e4072082907d80d84c93cbf9e1d9a7c7b96d9
Andy <andypham2341@hotmail.com>
2016-07-14 19:26:49 -0400

    Cttl

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,19 +2,36 @@ class PinsController < ApplicationController
 	before_action :f_pin, only: [:show,:edit,:update,:destroy]
 
 	def index
-		@pin = Pin.new
+		@pin = Pin.all
 	end
 	def show
 	end
 	def new
+		@pin = Pin.new
 	end
 	def create
+		@pins = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin :notice "Congrats!"
+		else
+			redirect_to 'new'
+		end
 	end
+
 	def edit
 	end
+
 	def update
+		if @pin.update(pin_params)
+			redirect_to @pin :notice "Congrats!"
+		else
+			redirect_to 'edit'
+		end
 	end
+
 	def destroy
+		@pin.destroy
+		redirect_to 'index'
 	end
 
 	private
